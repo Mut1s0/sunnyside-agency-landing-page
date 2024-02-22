4edba8030722666d250cc55443b9e434a9148193
Allan Musyoka <musyokaallan96@gmail.com>
2024-02-22 20:22:50 +0300

    Added services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,31 @@
+import { desktopGraphicDesign, mobileGraphicDesign, desktopPhotography, mobilePhotography } from "../images";
 
 function Services() {
     return(
-        <h1>Services</h1>
+        <section>
+            <div className="grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2">
+                <article className="relative">
+                    <picture>
+                        <source media="(min-width: 768px)" srcSet={desktopGraphicDesign} />
+                        <img src={mobileGraphicDesign} alt="Graphic Design" className="w-full" />
+                    </picture>
+                    <div className="absolute bottom-[25%] left-[25%] text-center w-[50%]">
+                        <h1 className="font-fraunces font-blackFraunces text-darkDesaturatedCyan text-[35px]">Graphic Design</h1>
+                        <p className="font-barlow font-semiBoldBarlow text-darkDesaturatedCyan text-[15px]">Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential client attention.</p>
+                    </div>
+                </article>
+                <article className="relative">
+                    <picture>
+                        <source media="(min-width: 768px)" srcSet={desktopPhotography} />
+                        <img src={mobilePhotography} alt="Graphic Design" className="w-full" />
+                    </picture>
+                    <div className="absolute bottom-[25%] left-[25%] text-center w-[50%]">
+                        <h1 className="font-fraunces font-blackFraunces text-darkBlue text-[35px]">Photography</h1>
+                        <p className="font-barlow font-semiBoldBarlow text-darkBlue text-[15px]">Increase your credibility by getting the most stunning high-quality photos that improve your business.</p>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,25 @@
+import { emily, jennie, thomas } from "../images"
+
+export const testimonials = [
+    {
+        id: "testimony-1",
+        icon: emily,
+        testimony: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+        name: "Emily R.",
+        position: "Marketing Director"
+    },
+    {
+        id: "testimony-2",
+        icon: thomas,
+        testimony: "Sunnyside enthusiasm coupled with their keen interest in our brands success made it a satisfying and enjoyable experience",
+        name: "Thomas S.",
+        position: "Chief Operating Officer"
+    },
+    {
+        id: "testimony-3",
+        icon: jennie,
+        testimony: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+        name: "Jennie F.",
+        position: "Business Owner"
+    },
+]
