f298dc27219a4b10f0d3d4ff6ee04c038ade832a
Allan Musyoka <musyokaallan96@gmail.com>
2024-02-22 11:38:18 +0300

    first commit

---
+++ src/images/index.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import iconArrowDown from "./icon-arrow-down.svg";
+import iconFacebook from "./icon-facebook.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import iconInstagram from "./icon-instagram.svg";
+import iconPinterest from "./icon-pinterest.svg";
+import iconTwitter from "./icon-twitter.svg";
+import emily from "./image-emily.jpg";
+import jennie from "./image-jennie.jpg";
+import thomas from "./image-thomas.jpg";
+
+// Desktop Images
+import desktopGalleryCone from "./desktop/image-gallery-cone.jpg";
+import desktopGalleryMilkbottles from "./desktop/image-gallery-milkbottles.jpg";
+import desktopGalleryOrange from "./desktop/image-gallery-orange.jpg";
+import desktopGallerySugarcubes from "./desktop/image-gallery-sugarcubes.jpg";
+import desktopGraphicDesign from "./desktop/image-graphic-design.jpg";
+import desktopHeader from "./desktop/image-header.jpg";
+import desktopPhotography from "./desktop/image-photography.jpg";
+import desktopStandOut from "./desktop/image-stand-out.jpg";
+import desktopTransform from "./desktop/image-transform.jpg";
+
+// Mobile Images
+import mobileGalleryCone from "./mobile/image-gallery-cone.jpg";
+import mobileGalleryMilkbottles from "./mobile/image-gallery-milkbottles.jpg";
+import mobileGalleryOrange from "./mobile/image-gallery-orange.jpg";
+import mobileGallerySugarcubes from "./mobile/image-gallery-sugarcubes.jpg";
+import mobileGraphicDesign from "./mobile/image-graphic-design.jpg";
+import mobileHeader from "./mobile/image-header.jpg";
+import mobilePhotography from "./mobile/image-photography.jpg";
+import mobileStandOut from "./mobile/image-stand-out.jpg";
+import mobileTransform from "./mobile/image-transform.jpg";
+
+export {
+    logo,
+    iconArrowDown,
+    iconFacebook,
+    iconHamburger,
+    iconInstagram,
+    iconPinterest,
+    iconTwitter,
+    emily,
+    jennie,
+    thomas,
+    desktopGalleryCone,
+    desktopGalleryMilkbottles,
+    desktopGalleryOrange,
+    desktopGallerySugarcubes,
+    desktopGraphicDesign,
+    desktopHeader,
+    desktopPhotography,
+    desktopStandOut,
+    desktopTransform,
+    mobileGalleryCone,
+    mobileGalleryMilkbottles,
+    mobileGalleryOrange,
+    mobileGallerySugarcubes,
+    mobileGraphicDesign,
+    mobileHeader,
+    mobilePhotography,
+    mobileStandOut,
+    mobileTransform
+}
