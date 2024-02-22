3cbdf8e30e988c7ec18c1e72ca92b6f040ace5b8
Allan Musyoka <musyokaallan96@gmail.com>
2024-02-22 21:22:54 +0300

    Added gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,35 @@
-
+import { desktopGalleryMilkbottles, mobileGalleryMilkbottles, desktopGalleryOrange, mobileGalleryOrange, desktopGalleryCone, mobileGalleryCone, desktopGallerySugarcubes, mobileGallerySugarcubes } from "../images";
 function Gallery() {
+
     return(
-        <h1>Gallery</h1>
+
+        <section>
+
+            <div className="grid grid-cols-1 ss:grid-cols-2 md:grid-cols-4">
+
+                <picture>
+                    <source media="(min-width: 768px)" srcSet={desktopGalleryMilkbottles} />
+                    <img src={mobileGalleryMilkbottles} alt="Milk Bottles" className="w-full" />
+                </picture>
+
+                <picture>
+                    <source media="(min-width: 768px)" srcSet={desktopGalleryOrange} />
+                    <img src={mobileGalleryOrange} alt="Orange" className="w-full" />
+                </picture>
+
+                <picture>
+                    <source media="(min-width: 768px)" srcSet={desktopGalleryCone} />
+                    <img src={mobileGalleryCone} alt="Cone" className="w-full" />
+                </picture>
+
+                <picture>
+                    <source media="(min-width: 768px)" srcSet={desktopGallerySugarcubes} />
+                    <img src={mobileGallerySugarcubes} alt="Sugar Cubes" className="w-full" />
+                </picture>
+
+            </div>
+
+        </section>
     )
 }
 
